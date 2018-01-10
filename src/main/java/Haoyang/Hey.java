a1a99063e173fe6da38743b607fb849854cbc425
50379 <50379@DESKTOP-JS50U1Q>
2018-01-10 12:12:08 +0800

    add

---
+++ src/main/java/Haoyang/Hey.java
@@ -0,0 +1,20 @@
+/**
+ * Project Name:SIXwork
+ * File Name:Hey.java
+ * Package Name:Haoyang
+ * Date:2018年1月10日上午9:25:38
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package Haoyang;
+/**
+ * Description:   <br/>
+ * Date:     2018年1月10日 上午9:25:38 <br/>
+ * @author   WangHaoyang
+ * @version
+ * @see
+ */
+public class Hey {
+
+}
+
---
+++ src/main/java/Hw20180106.java
@@ -0,0 +1,28 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Project Name:SIXwork
+ * File Name:Hw20180106.java
+ * Package Name:
+ * Date:2018年1月7日下午5:47:00
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ *
+ */
+
+/**
+ * Description:	   <br/>
+ * Date:     2018年1月7日 下午5:47:00 <br/>
+ * @author   dingP
+ * @version  
+ * @see 	 
+ */
+public class Hw20180106 {
+          public static void main(String[] args) throws IOException {
+            FileOutputStream fos = new FileOutputStream("D:/lession7homework.java");
+        }
+
+       
+}
+
