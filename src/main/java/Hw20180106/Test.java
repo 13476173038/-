0c7f8060af81cd598b7b9a224ad2570f1806f2e8
13476173038 <392564094@qq.com>
2018-03-26 09:54:54 +0800

    爱到底

--- src/main/java/Hw20180106/Test.java
+++ src/main/java/Hw20180106/Test.java
@@ -5,7 +5,7 @@
  * Date:2018年1月6日下午4:03:23
  * Copyright (c) 2018, bluemobi All Rights Reserved.
  *
-*/
+ */
 
 package Hw20180106;
 
@@ -14,49 +14,39 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.apache.log4j.Logger;
-
-
-
 /**
- * Description:	   <br/>
- * Date:     2018年1月6日 下午4:03:23 <br/>
- * @author   mingX
- * @version  
- * @see 	 
+ * Description: <br/>
+ * Date: 2018年1月6日 下午4:03:23 <br/>
+ * 
+ * @author mingX
+ * @version
+ * @see
  */
 public class Test {
-    private final static Logger LOG = Logger.getLogger(Test.class);
 
-   
-     public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         String path = "C:/lingxing.java";
         path = "D:/lession7homework.java";
         File file = new File(path);
         FileInputStream fis = null;
-        try{
+        try {
             fis = new FileInputStream(file);
             byte[] b = new byte[2];
             int len = fis.read(b);
-            System.out.println(len+"len等于");
-            while(len!=1){
-                String str = new String(b,0,len);
-                LOG.info("\n"+str);
+            System.out.println(len + "len等于");
+            while (len != 1) {
+                String str = new String(b, 0, len);
                 len = fis.read(b);
             }
-        
-        }catch(FileNotFoundException e){
-         
-            LOG.info("fnff",e);
-            
-            }catch(IOException e){
-                LOG.info("IO",e);
-            }finally{
-            if(fis!=null){
+
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
+        } finally {
+            if (fis != null) {
                 fis.close();
             }
-            }
         }
+    }
 }
-
