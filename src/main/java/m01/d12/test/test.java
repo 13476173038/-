2758a01aa8b17c536ba9eb36d0d51216eebb96da
13476173038 <392564094@qq.com>
2018-03-26 09:11:16 +0800

    add

---
+++ src/main/java/m01/d12/test/test.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:SIXwork
+ * File Name:test.java
+ * Package Name:m01.d12.test
+ * Date:2018年3月26日上午9:09:43
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package m01.d12.test;
+
+/**
+ * Description: <br/>
+ * Date: 2018年3月26日 上午9:09:43 <br/>
+ * 
+ * @author sunhao
+ * @version
+ * @see
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
