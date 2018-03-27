4471416c86b5edec341a8add02f34e50b2e85279
hupeng1202 <1753023600@qq.com>
2018-03-27 22:59:03 +0800

    hupeng

---
+++ src/main/mysql/m03/d27/hupeng/ProvinceCity.java
@@ -0,0 +1,55 @@
+/**
+ * Project Name:LianXi.Demo
+ * File Name:Province.java
+ * Package Name:jdbc
+ * Date:2018年3月27日下午8:43:05
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package jdbc;
+/**
+ * Description:   <br/>
+ * Date:     2018年3月27日 下午8:43:05 <br/>
+ * @author   hupeng
+ * @version
+ * @see
+ */
+public class ProvinceCity {
+   private int p_pid;
+   private String pname;
+   private int cid;
+   private String cname;
+   private int c_pid;
+public int getP_pid() {
+	return p_pid;
+}
+public void setP_pid(int p_pid) {
+	this.p_pid = p_pid;
+}
+public String getPname() {
+	return pname;
+}
+public void setPname(String pname) {
+	this.pname = pname;
+}
+public int getCid() {
+	return cid;
+}
+public void setCid(int cid) {
+	this.cid = cid;
+}
+public String getCname() {
+	return cname;
+}
+public void setCname(String cname) {
+	this.cname = cname;
+}
+public int getC_pid() {
+	return c_pid;
+}
+public void setC_pid(int c_pid) {
+	this.c_pid = c_pid;
+}
+   
+}
+
---
+++ src/main/mysql/m03/d27/hupeng/jdbcSelect1.java
@@ -0,0 +1,99 @@
+/**
+ * Project Name:LianXi.Demo
+ * File Name:jdbcSelect1.java
+ * Package Name:jdbc
+ * Date:2018年3月27日下午8:30:09
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description:   <br/>
+ * Date:     2018年3月27日 下午8:30:09 <br/>
+ * @author   hupeng
+ * @version
+ * @see
+ */
+public class jdbcSelect1 {
+	public static void main(String[] args) {
+		jdbcSelect1  jbs=new jdbcSelect1();
+		List<ProvinceCity> list=jbs.getAllProvince();
+		for (ProvinceCity p : list) {
+			System.out.println(p.getCid()+p.getCname()+p.getPname());
+		}
+		
+	}
+	Connection con=null;
+	PreparedStatement pst=null;
+	ResultSet  rs=null;
+	private String url="jdbc:mysql://localhost:3306/mydata";
+	
+	public Connection getCon(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection(url, "root","root");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		
+		return con;
+		
+	}
+	public List<ProvinceCity>  getAllProvince(){
+		List<ProvinceCity>  list=new ArrayList<ProvinceCity>(); 
+		try {
+			String sql="SELECT b.`cid`,b.`cname`,a.`pname` FROM province a INNER JOIN city b ON a.`pid`=b.`pid`";
+			pst=getCon().prepareStatement(sql);
+			rs=pst.executeQuery();
+			while(rs.next()){
+				ProvinceCity p=new 	ProvinceCity();
+				p.setCid(rs.getInt("cid"));
+				p.setCname(rs.getString("cname"));
+				p.setPname(rs.getString("pname"));
+				p.setC_pid(rs.getInt("c_pid"));
+				p.setP_pid(rs.getInt("p_pid"));
+				list.add(p);
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exceptio
+			e.printStackTrace();
+  
+		}finally{
+			close(con,pst,rs);
+		}
+		return null;
+	}
+	private void close(Connection con, PreparedStatement pst, ResultSet rs) {
+	
+			try {
+				if (null!=con) {
+				con.close();
+				}if (null !=pst) {
+					pst.close();
+				}if (null !=rs) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				
+				
+				e.printStackTrace();
+				
+			}
+		
+	}
+
+}
+
