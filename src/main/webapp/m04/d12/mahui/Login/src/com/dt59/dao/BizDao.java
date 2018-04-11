d4bee6c7c94f426aecab7ad3a7db41387b015412
MaHuiGoldenAge <mahuigoldenage@163.com>
2018-04-12 06:17:57 +0800

    twice work of news

---
+++ src/main/webapp/m04/d12/mahui/Login/src/com/dt59/dao/BizDao.java
@@ -0,0 +1,12 @@
+package com.dt59.dao;
+
+import java.util.List;
+
+import com.dt59.entity.NewsInfo;
+import com.dt59.entity.UserInfo;
+
+public interface BizDao {
+	boolean validateUser(UserInfo user);
+	
+	List<NewsInfo> getNews();
+}
