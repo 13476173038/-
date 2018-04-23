0e662bf4dd8b1d6b06d1ec6e1d24f299c7f0163a
13476173038 <392564094@qq.com>
2018-04-24 00:01:54 +0800

    add

---
+++ src/main/webapp/m04/d23/sunhao/AjaxServlet.java
@@ -0,0 +1,49 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxServlet
+ */
+@WebServlet("/AjaxServlet")
+public class AjaxServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjaxServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html");
+        String city = request.getParameter("a");
+        PrintWriter out = response.getWriter();
+        if (city.equals("")) {
+            out.print("«Î—°‘Ò≥« –");
+        }
+        out.flush();
+        out.close();
+    }
+
+}
